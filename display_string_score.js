169d367b86fcfd2163579d766bb0cac1aace5000
Kelong Cong <kelongcong@gmail.com>
2013-03-13 18:37:07 +0000

    implemented added and unadded list feature

--- display_string_score.js
+++ display_string_score.js
@@ -102,8 +102,8 @@ function show_results(){
   if (ranked){
     sortResults('rank', false);
     for (i = 0; i < items.length; i+=1){
-      if (items[i].rank > 0){
-        update_checkbox(i);
+      if (items[i].rank > 0 && items[i].checked === false){
+        populate_item(i);
         child_count += 1;
       }
     }
@@ -112,47 +112,73 @@ function show_results(){
   } else {
     sortResults('f_name', true);
     for (i = 0; i < items.length; i+=1){
-      update_checkbox(i);
-      child_count += 1;
+      if (items[i].checked === false) {
+        populate_item(i);
+        child_count += 1;
+      }
     }
   }
 
   // display sorry not found message if there are no results
   if (child_count === 0) {
-    $('#itemsdiv').append('<b>sorry, not found</b><br />');
+    $('#itemsdiv')
+      .append('<b>sorry, not found</b><br />')
+      .hide().fadeIn(300);
   }
 }
 
-function update_checkbox(i){
+function populate_item(i){
 
-  var selector = 'input[id="chkbox_' + i + '"]';
+  // need to match with #itemsdiv child
+  var selector = 'a[id="chkbox_' + i + '"]';
 
   full_name = items[i].f_name + ' ' + items[i].l_name;
-  $('#itemsdiv').append('<input type="checkbox" id="chkbox_' +
-    i + '" onclick="checkbox_action(' + i + ')">' + full_name + '</input><br/>');
+  // $('#itemsdiv').append('<input type="checkbox" id="chkbox_' +
+  //   i + '" onclick="checkbox_action(' + i + ')">' + full_name + '</input><br/>');
+  $('#itemsdiv')
+    .append('<div id="notselected_' + i + '"><a href="#" onclick="add_action(' + i + ')">(+)</a> ' + full_name + '<br/></div>');
+    // .hide().fadeIn(300);
 
-  // check the checked JSON attribute 
-  if (items[i].checked === true){
-    $(selector).attr('checked', true);
-    // console.log('update_checkbox: checked!');
-  } else {
-    $(selector).attr('checked', false);
-    // console.log('update_checkbox: not checked!');
-  }
+  // // check the checked JSON attribute 
+  // if (items[i].checked === true){
+  //   $(selector).attr('checked', true);
+  //   // console.log('update_checkbox: checked!');
+  // } else {
+  //   $(selector).attr('checked', false);
+  //   // console.log('update_checkbox: not checked!');
+  // }
 }
 
-function checkbox_action(i){
+function add_action(i){
 
   full_name = items[i].f_name + ' ' + items[i].l_name;
-  if ($('input[id="chkbox_' + i + '"]').is(':checked')){
+
+  // need to match with #itemsdiv child
+  if (items[i].checked === false){
     items[i].checked = true;
-    $('#selected').append('<span id=selected_' + items[i].id + '>' + full_name + ', </span>');
+    $('#selected')
+      .append('<div id=selected_' + items[i].id + '><a href="#" onclick="del_action(' + items[i].id + ');">(-)</a>' + full_name + '</div>');
+      // .hide().fadeIn(300);
   } else {
-    items[i].checked = false;
-    $('#selected_' + items[i].id).remove();
-    // console.log('action: unchecked!');
+    console.log('add_action: error!');
   }
+
+  $('#notselected_' + i).fadeOut("normal", function(){ $(this).remove(); });
   // items[i].checked = $('input[id="chkbox_' + i + '"]').is(':checked') ? true : false;
+}
+
+function del_action(id){
+
+  // the i here is the id attribute
+  for (var i = 0; i < items.length; i+=1){
+    if (items[i].id === id){
+      items[i].checked = false;
+    }
+  }
+  $('#selected_' + id).fadeOut("normal", function(){ $(this).remove(); });
+
+  rank_display_results();
+
 }
 
 // taken from
